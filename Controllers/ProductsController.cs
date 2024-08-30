3063e3fd690c48bb642e99abb85d1003130eef7d
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-08-30 12:17:00 +0300

    edit Index.cshtml

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using BestShopIT.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestShopIT.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        //Connection to the Database
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
