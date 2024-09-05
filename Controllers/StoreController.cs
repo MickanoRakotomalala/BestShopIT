30f67d088e7a8d1efcf0952cc3aa7fbc4364b99a
Mickano <Mickano@DESKTOP-D452MTK>
2024-09-05 06:35:02 +0300

    StoreController and View

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,22 @@
+﻿using BestShopIT.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestShopIT.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public StoreController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p => p.Id).ToList();
+
+            ViewBag.Products = products;
+            return View();
+        }
+    }
+}
