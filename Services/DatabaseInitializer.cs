cf278cef3508759333ebfb9660fb3d01f149c820
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-09-05 15:11:57 +0300

    Create Roles and Default Admin user

--- Services/DatabaseInitializer.cs
+++ Services/DatabaseInitializer.cs
@@ -1,12 +1,11 @@
-﻿using BestShopIT.Migrations;
+﻿using BestShopIT.Models;
 using Microsoft.AspNetCore.Identity;
-using BestShopIT.Models;
 
 namespace BestShopIT.Services
 {
     public class DatabaseInitializer
     {
-        public static async Task SeedDataAsync(UserManager<Models.ApplicationUser>? userManager,
+        public static async Task SeedDataAsync(UserManager<ApplicationUser>? userManager,
             RoleManager<IdentityRole>? roleManager)
         {
             if(userManager == null || roleManager == null)
@@ -15,59 +14,59 @@ namespace BestShopIT.Services
                 return;
             }
 
-            // check if we have the admin role or not
+            //check if we have the admin role or not
             var exists = await roleManager.RoleExistsAsync("admin");
             if (!exists) 
             {
-                Console.WriteLine("Admin role is not definied and will be created");
+                Console.WriteLine("Admin role is not defined and will be created");
                 await roleManager.CreateAsync(new IdentityRole("admin"));
             }
 
-            // check if we have the seller role or not
+            //check if we have the seller role or not
             exists = await roleManager.RoleExistsAsync("seller");
             if (!exists)
             {
-                Console.WriteLine("Seller role is not definied and will be created");
+                Console.WriteLine("Seller role is not defined and will be created");
                 await roleManager.CreateAsync(new IdentityRole("seller"));
             }
 
-            // check if we have the client role or not
+            //check if we have the client role or not
             exists = await roleManager.RoleExistsAsync("client");
             if (!exists)
             {
-                Console.WriteLine("Client role is not definied and will be created");
+                Console.WriteLine("Client role is not defined and will be created");
                 await roleManager.CreateAsync(new IdentityRole("client"));
             }
 
-            // check if we have at least one admin user or not
+            //check if we have at least one admin or user not
             var adminUsers = await userManager.GetUsersInRoleAsync("admin");
-            if (adminUsers.Any())
+            if (adminUsers.Any()) 
             {
-                //Admin user Already exists => exit
+                //Admin user already exists => exit
                 Console.WriteLine("Admin user already exists => exit");
                 return;
             }
 
-            // create the admin user
-            var user = new Models.ApplicationUser()
+            //Create the admin user
+            var user = new ApplicationUser()
             {
-                FirstName = "Admin",
-                LastName = "Admin",
-                UserName = "admin@admin.com", //UserName will be used to authenticate the user
+                FirstName = "admin",
+                LastName = "admin",
+                UserName = "admin@admin.com",
                 Email = "admin@admin.com",
                 CreatedAt = DateTime.Now,
             };
 
             string initialPassword = "admin123";
 
-            var result =  await userManager.CreateAsync(user, initialPassword);
+            var result = await userManager.CreateAsync(user, initialPassword);
             if (result.Succeeded) 
             {
-                //set the user role
-                await userManager.AddToRoleAsync(user, "Admin");
+                // set the user role
+                await userManager.AddToRoleAsync(user, "admin");
                 Console.WriteLine("Admin user created successfully! Please update the initial password!");
-                Console.WriteLine("Email: " + user.Email);
-                Console.WriteLine("Initial password: " + initialPassword);
+                Console.WriteLine("Email :" + user.Email);
+                Console.WriteLine("Initial password : " + initialPassword);
             }
         }
     }
