48eaff877bf35fe158d58abac0681a6f499c6986
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-09-10 16:46:40 +0300

    Update RegisterDto

--- Models/RegisterDto.cs
+++ Models/RegisterDto.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BestShopIT.Models
 {
