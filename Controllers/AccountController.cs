cd238c06138201b6f9441f2aa875fe01fa37bc7c
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-09-11 11:35:45 +0300

    User authentication

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -17,12 +17,21 @@ namespace BestShopIT.Controllers
         }
         public IActionResult Register()
         {
+            if(signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> Register(RegisterDto registerDto)
         {
+            if(signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
             if (!ModelState.IsValid) 
             {
                 return View(registerDto);
@@ -69,6 +78,43 @@ namespace BestShopIT.Controllers
                 await signInManager.SignOutAsync();
             }
             return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Login()
+        {
+            if(signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if(signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(loginDto);
+            }
+
+            var result = await signInManager.PasswordSignInAsync(loginDto.Email, loginDto.Password, loginDto.RememberMe, false);
+            if (result.Succeeded) 
+            {
+                return RedirectToAction("Index", "Home");
+                
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid login attempt.";
+            }
+
+            return View(loginDto);
         }
     }
 }
