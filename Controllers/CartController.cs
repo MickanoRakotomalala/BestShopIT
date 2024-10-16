c5844744f992d967b3c66ee2da8a8ce593e80ddf
Mickano <rakotomalalamickano@gmail.com>
2024-10-16 17:12:34 +0300

    Shopping Cart - Display Items

---
+++ Controllers/CartController.cs
@@ -0,0 +1,34 @@
+﻿using BestShopIT.Models;
+using BestShopIT.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestShopIT.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly decimal ShippingFee;
+
+        public CartController(ApplicationDbContext context, IConfiguration configuration, UserManager<ApplicationUser> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+            ShippingFee = configuration.GetValue<decimal>("CartSettings:ShippingFee");
+        }
+
+        public IActionResult Index()
+        {
+            List<OrderItem> cartItems = CartHelper.GetCartItems(Request, Response, context);
+            decimal subtotal = CartHelper.GetSubtotal(cartItems);
+
+            ViewBag.CartItems = cartItems;
+            ViewBag.ShippingFee = ShippingFee;
+            ViewBag.Subtotal = subtotal;
+            ViewBag.Total = subtotal + ShippingFee;
+
+            return View();
+        }
+    }
+}
