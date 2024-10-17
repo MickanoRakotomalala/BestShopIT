75f291597ddd0f9b2c6d2227e1ce6554a417cd5e
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-10-17 16:35:05 +0300

    Pagination orders

--- Controllers/AdminOrdersController.cs
+++ Controllers/AdminOrdersController.cs
@@ -1,4 +1,5 @@
-﻿using BestShopIT.Services;
+﻿using BestShopIT.Models;
+using BestShopIT.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,16 +11,31 @@ namespace BestShopIT.Controllers
     public class AdminOrdersController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly int pageSize = 2;
 
         public AdminOrdersController(ApplicationDbContext context)
         {
             this.context = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(int pageIndex)
         {
-            var orders = context.Orders.Include(o => o.Client).Include(o => o.Items).OrderByDescending(o => o.Id).ToList();
+            IQueryable<Order> query = context.Orders.Include(o => o.Client).Include(o => o.Items).OrderByDescending(o => o.Id);
 
+            if (pageIndex <= 0) 
+            {
+                pageIndex = 1;
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / pageSize);
+
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+            var orders = query.ToList();
             ViewBag.Orders = orders;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+
             return View();
         }
     }
