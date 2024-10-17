8647ee17bc2ef30a67c4b8dd7af222819288748d
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-10-17 16:14:42 +0300

    Admin Displays Order

---
+++ Controllers/AdminOrdersController.cs
@@ -0,0 +1,26 @@
+﻿using BestShopIT.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestShopIT.Controllers
+{
+    [Authorize(Roles ="admin")]
+    [Route("/Admin/Orders/{action=Index}/{id?}")]
+    public class AdminOrdersController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public AdminOrdersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var orders = context.Orders.Include(o => o.Client).Include(o => o.Items).OrderByDescending(o => o.Id).ToList();
+
+            ViewBag.Orders = orders;
+            return View();
+        }
+    }
+}
