e3da247ad1889fdce396815e363ee2f5f40aa815
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-10-24 16:55:04 +0300

    Up Nav

--- Models/StoreSearchModel.cs
+++ Models/StoreSearchModel.cs
@@ -2,9 +2,9 @@
 {
     public class StoreSearchModel
     {
-        public string? Search { get; set; }
-        public string? Brand { get; set; }
-        public string? Category {  get; set; }
-        public string? Sort {  get; set; }
+        public string? Search { get; set; } = "";
+        public string? Brand { get; set; } = "";
+        public string? Category { get; set; } = "";
+        public string? Sort { get; set; } = "";
     }
 }
