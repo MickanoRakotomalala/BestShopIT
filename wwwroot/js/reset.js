d171909698340c21795c3f287a79ea3a329056cd
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-10-25 14:49:39 +0300

    Update Form Reset Password

---
+++ wwwroot/js/reset.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const resetPassword = document.getElementById('resetpw');
+ 
+    document.getElementById('togglerespw').addEventListener('click', function () {
+        const typeresetpassword = resetPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+        resetPassword.setAttribute('type', typeresetpassword);
+
+        this.classList.toggle('bi-eye');
+        this.classList.toggle('bi-eye-slash');
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const resetPassword = document.getElementById('cresetpw');
+
+    document.getElementById('togglecrpw').addEventListener('click', function () {
+        const typeresetpassword = resetPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+        resetPassword.setAttribute('type', typeresetpassword);
+
+        this.classList.toggle('bi-eye');
+        this.classList.toggle('bi-eye-slash');
+    })
+});
